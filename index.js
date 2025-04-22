cadebf550e47231a08a870eda73d540105b8d72e
Alkis <alkisax@gmail.com>
2025-04-22 23:18:02 +0300

    deploy

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const port = 3000
 
 // const YOUR_DOMAIN = 'http://localhost:3000';
 // const YOUR_DOMAIN = 'https://stripebasic.onrender.com'
-res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+
 
 // const PRICE_ID_050 = 'price_1RGPe4EsaPshQGwV6vXbMrhE'
 // const PRICE_ID_051 = 'price_1RGkyMEsaPshQGwV7rsnw60y'
@@ -32,7 +32,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //   res.send('Hello World!')
 // })
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve('dist/index.html'))
+  // res.sendFile(path.resolve('dist/index.html'))
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 app.post('/checkout/:price_id', async (req, res) => {
